91852fafb4074a0bcddaf4244d41fb90e6b9a3a8
john-royal <34844819+john-royal@users.noreply.github.com>
2023-05-20 17:46:25 -0400

    Update Dijkstra and diameter

--- graph-package/graph-algorithms/dijkstra.scm
+++ graph-package/graph-algorithms/dijkstra.scm
@@ -1,71 +1,78 @@
 ; graph-algorithms/dijkstra.scm
 
 
-(load "../basic-data-types/adjacency-list.scm")
+(load "../basic-data-types/graph.scm")
 (load "../basic-data-types/pair.scm")
 (load "../util.scm")
 
+(define (insertion-sort lst less-than?)
+  (define (insert x sorted-lst)
+    (if (null? sorted-lst)
+        (list x)
+        (if (less-than? x (car sorted-lst))
+            (cons x sorted-lst)
+            (cons (car sorted-lst) (insert x (cdr sorted-lst))))))
+  (if (null? lst)
+      '()
+      (insert (car lst) (insertion-sort (cdr lst) less-than?))))
 
-; Returns all edges connected to the given node.
-(define (find-edges-from-node node graph)
-  (filter (lambda (edge) (equal? (edge-src edge) node)) graph))
+(define (find-first pred lst)
+  (cond ((null? lst) #f)
+        ((pred (car lst)) (car lst))
+        (else (find-first pred (cdr lst)))))
 
+(define (ascending-distances? node1 node2)
+  (let ((d1 (second node1))
+        (d2 (second node2)))
+    (cond ((eq? d1 #f) #f)
+          ((eq? d2 #f) #t)
+          (else (< d1 d2)))))
 
-; I was going to try and explain this, but I’m tired. I’ll try again some other time.
-(define (update-distances-from-node current graph distances)
-  (let* ((node (node current))                      ; current node
-         (weight (node-weight current))             ; weight of current node
-         (edges (find-edges-from-node node graph))) ; edges reachable from current node
-    ; Analyze each edge from this node to find the shortest path to each destination node
-    (accumulate
-     (lambda (edge distances)
-       (let* ((dest (edge-dest edge))
-              (edge-weight (edge-weight edge))
-              (total-weight (+ weight edge-weight)))
-         ; Record the length of this edge if it’s the shortest path to this node that we can find.
-         (if (assoc dest distances)
-             ; Compare the length of this edge to the length we already have for this node.
-             (if (< total-weight (node-weight (assoc dest distances)))
-                 ; If this edge is shorter, replace the saved length with this one.
-                 (cons (make-node dest total-weight) (filter (lambda (x) (equal? (node x) dest)) distances))
-                 ; Otherwise, use the length we already have (i.e. do not modify the list).
-                 distances)
-             ; If we don’t have a length for this node, use the one from this edge.
-             (cons (make-node dest total-weight) distances))))
-     distances
-     edges)))
+(define (initialize-distances graph start)
+  (insertion-sort
+   (map (lambda (node) (if (eq? node start) (pair start 0) (pair node #f))) (nodes graph))
+   ascending-distances?))
 
+(define (compute-neighbor-distances distance neighbors)
+    (map (lambda (neighbor)
+           (let ((node (if (pair? neighbor) (car neighbor) neighbor))
+                 (length (if (pair? neighbor) (cadr neighbor) 1)))
+             (pair node (+ distance length))))
+       neighbors))
 
-; Returns the length of the shortest path between a source node and a destination node.
-; If no such path exists, returns #f.
-(define (dijkstra graph src dest)
-  (let iter ((queue (list (make-node src 0)))      ; nodes that need to be processed, beginning with source
-             (visited '())                         ; nodes that have already been processed
-             (distances (list (make-node src 0)))) ; distances from source to all other nodes
-    ; If the queue is empty and we haven't found the destination, there's no path.
-    (if (null? queue)
-        #f
-        ; Find the node with the shortest distance from the queue.
-        (let* ((queue (sort (lambda (x y) (< (node-weight x) (node-weight y))) queue))
-               (current (car queue))
-               (queue (cdr queue)))
-          ; If the current node is the destination, return the associated length — this is the shortest path.
-          (cond ((equal? (node current) dest) (node-weight current))
-                ; If we've already visited this node, continue to the next one.
-                ((member (node current) visited) (iter queue visited distances))
-                ; Otherwise, process distances from the current node and add the node to the visited list.
-                (else (iter (append queue (update-distances-from-node current graph distances))
-                            (cons (node current) visited)
-                            (update-distances-from-node current graph distances))))))))
+(define (insert-distance node-distance lst)
+  (cond ((null? lst) (list node-distance))
+        ((eq? (first node-distance) (first (car lst)))
+         (if (ascending-distances? node-distance (car lst))
+             (cons node-distance (cdr lst))
+             lst))
+        (else (cons (car lst) (insert-distance node-distance (cdr lst))))))
 
+(define (merge-distances lst1 lst2)
+  (if (null? lst1)
+      (insertion-sort lst2 ascending-distances?)
+      (merge-distances (cdr lst1) (insert-distance (car lst1) lst2))))
 
-(define graph
-  (list (make-edge 'A 'B 1)
-        (make-edge 'B 'A 1)
-        (make-edge 'A 'C 3)
-        (make-edge 'C 'A 3)
-        (make-edge 'B 'C 2)
-        (make-edge 'C 'B 2)))
+(define (dijkstra graph start end)
+  (let iter ((unvisited (nodes graph))
+             (distances (initialize-distances graph start)))
+    (if (null? unvisited) #f
+        (let* ((node-distance (find-first (lambda (node-distance) (set-member? (car node-distance) unvisited)) distances))
+               (node (car node-distance))
+               (distance (cadr node-distance)))
+          (if (eq? node end) distance
+              (let ((neighbor-distances (compute-neighbor-distances (if (not distance) 0 distance) (neighbors graph node))))
+                (iter (set-remove node unvisited)
+                      (merge-distances neighbor-distances distances))))))))
 
-(dijkstra graph 'A 'B) ; 1
+(define g (make-empty-graph))
+(define g (add-node g 'A))
+(define g (add-node g 'B))
+(define g (add-node g 'C))
+(define g (add-node g 'D))
+(define g (add-edge g (make-edge 'A 'B 1)))
+(define g (add-edge g (make-edge 'A 'C 4)))
+(define g (add-edge g (make-edge 'B 'C 2.5)))
+
+(dijkstra g 'A 'C) ; 3.5
 
