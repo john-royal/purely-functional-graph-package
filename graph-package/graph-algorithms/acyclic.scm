8a891a8f17a75f130523561c42e2c18edcff82f2
zikrya <77081809+zikrya@users.noreply.github.com>
2023-05-20 22:53:21 -0400

    Update acyclic.scm
    Fixed acyclic, should work now

--- graph-package/graph-algorithms/acyclic.scm
+++ graph-package/graph-algorithms/acyclic.scm
@@ -1,28 +1,32 @@
 ; Determine if a graph if acyclic.
-; This currently doesn’t work correctly.
 
 (load "graph-package/basic-data-types/set.scm")
-(load "graph-package/basic-data-types/graph.scm")
+(load "graph-package/basic-data-types/pair.scm")
+(load "graph-package/util.scm")
 
-(define (dfs-acyclic current_vertex parent graph visited)
-  (if (set-member? current_vertex visited)
-      (if (equal? parent current_vertex)
-          #f
-          #t)
-      (let loop ((neighbors (neighbors graph current_vertex)))
-        (if (null? neighbors)
-            #f
-            (let ((neighbor (car neighbors)))
-              (if (not (equal? parent neighbor))
-                  (if (dfs-acyclic neighbor current_vertex graph (set-insert current_vertex visited))
-                      #t
-                      (loop (cdr neighbors)))
-                  (loop (cdr neighbors))))))))
+; restructuring the DFS
+; So I removed the parent parameter
+; The function will return false if it sees a node alread in the visited list
+; It loops over all the neighbors of the current node, to check if it leads to a cycle
+; The biggest issue was the parents, I'm glad we removed it, didn't like it anyways, but originall thought we needed it
+
+(define (dfs-acyclic? graph current-node visited)
+  (let ((neighbors (neighbors graph current-node)))
+    (cond ((set-member? current-node visited) #f)   
+          ((null? neighbors) #t)                    
+          (else 
+            (let loop ((rest-neighbors neighbors)) 
+              (if (null? rest-neighbors)
+                  #t  
+                  (let ((next-node (car rest-neighbors)))
+                    (if (dfs-acyclic? graph next-node (cons current-node visited))
+                        (loop (cdr rest-neighbors))
+                        #f))))))))
 
 (define (acyclic? graph)
-  (let ((starting_vertex (first (nodes graph)))) 
-    (dfs-acyclic starting_vertex #f graph (set-create-empty))))
+  (dfs-acyclic? graph (car (nodes graph)) '()))
 
+; Test 1
 (define g (make-empty-graph))
 (define g (add-node g 'C))
 (define g (add-node g 'B))
@@ -32,4 +36,27 @@
 (define g (add-edge g (make-edge 'B 'A)))
 (define g (add-edge g (make-edge 'C 'B)))
 (display g)
-(display (acyclic? g)) ; should be #f, but is #t
+(newline)
+(newline)
+(acyclic? g); should be #f, but is #t
+(newline)
+(newline)
+
+; Test 2
+
+(define h (make-empty-graph))
+(define h (add-node h 'D))
+(define h (add-node h 'E))
+(define h (add-node h 'F))
+(define h (add-node h 'G))
+(define h (add-edge h (make-edge 'D 'E)))
+(define h (add-edge h (make-edge 'E 'F)))
+(define h (add-edge h (make-edge 'F 'G)))
+(display h)
+(newline)
+(newline)
+(acyclic? h)
+(newline)
+(newline)
+
+; Awesome it works now!
